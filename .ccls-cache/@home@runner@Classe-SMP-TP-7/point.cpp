5a06079d8b4b2937f4cf114c61e0586272b3332d
Kadwalon <77363726+K40NN@users.noreply.github.com>
2023-03-22 12:48:39 +0000

    ajout de la classe point

---
+++ .ccls-cache/@home@runner@Classe-SMP-TP-7/point.cpp
@@ -0,0 +1,5 @@
+#include "point.h"
+#include <iostream>
+using namespace std;
+point pt;
+std::cout << pt.X << endl;
---
+++ .ccls-cache/@home@runner@Classe-SMP-TP-7/point.h
@@ -0,0 +1,14 @@
+class point {
+private:
+  int X;
+  int Y;
+
+public:
+  void translater(point pt){};
+  void translater(double a, double b ){};
+
+  point() {
+    this->X = 0;
+    this->Y = 0;
+  }
+};
